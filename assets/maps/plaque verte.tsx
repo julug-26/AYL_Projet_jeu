f2b972763719b753cd6ab1ea60719d6fac2e7379
nael.belhadj-amara <nael.belhadj-amara@epita.fr>
2026-05-23 20:50:27 +0200

    changer fichier tsx pour les mettre dans fichier tiled comme donjon 1

--- assets/maps/plaque verte.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="plaque verte" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="D:/ChatGPT Image 23 mai 2026, 18_53_52.png" width="1024" height="1024"/>
-</tileset>
