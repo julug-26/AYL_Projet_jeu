a4420983d4a47000b5f7ecb54a921181ca5061ad
julia <julu.guimaraes.aline@gmail.com>
2026-05-25 00:51:02 +0200

    exec 2

---
+++ dist/IA/settings.py
@@ -0,0 +1,16 @@
+TITLE = "Demo IA"
+SCREEN_WIDTH  = 0  # sera défini dynamiquement
+SCREEN_HEIGHT = 0
+FPS = 60
+
+TILE_SIZE = 32
+
+WHITE  = (255, 255, 255)
+BLACK  = (0,   0,   0  )
+RED    = (220, 50,  50 )
+BLUE   = (50,  100, 220)
+GREEN  = (50,  200, 50 )
+YELLOW = (255, 255, 0  )
+GREY   = (100, 100, 100)
+
+WALL_LAYER = "wall"
